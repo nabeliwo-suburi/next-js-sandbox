eeadbaf21a2deaabfc19e2dc313e69b79b0dc762
nabeliwo <hiroki.c.watanabe@gmail.com>
2018-03-08 16:18:09 +0900

    end qiita tutorial

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>About page</title>
+    </Head>
+    <h1>about us</h1>
+  </div>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,29 @@
 import React from 'react'
-export default () => <div>Hello world!</div>
+import Head from 'next/head'
+
+const delay = (time, env) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(env)
+    }, time)
+  })
+}
+
+export default class Index extends React.Component {
+  static async getInitialProps ({ res }) {
+    const env = res ? 'server' : 'client'
+    const obj = await delay(1000, { env })
+    return obj
+  }
+
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>Index page</title>
+        </Head>
+        <h1>hello world! {this.props.env}</h1>
+      </div>
+    )
+  }
+}
